a7d773315ab1fb43f22cf1cec18085f4052e909e
Laura Thomson <lrthmsn@gmail.com>
2021-09-08 16:50:37 +0300

    Refactoring

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -7,14 +7,14 @@ from mock import patch, Mock
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-class ConnectionTestClass(TestCase):
+class DatabaseTestClass(TestCase):
 
     @staticmethod
     @patch('logging.info')
     @patch('sqlalchemy.orm.sessionmaker')
     def test_commit_changes_false(mock_session, mock_info):
         mock_instance = mock_session()
-        seq_dbutils.Connection.commit_changes(mock_instance, False)
+        seq_dbutils.Database.commit_changes(mock_instance, False)
         mock_info.assert_called_with('Changes NOT committed')
 
     @staticmethod
@@ -22,7 +22,7 @@ class ConnectionTestClass(TestCase):
     @patch('sqlalchemy.orm.sessionmaker')
     def test_commit_changes_true(mock_session, mock_info):
         mock_instance = mock_session()
-        seq_dbutils.Connection.commit_changes(mock_instance, True)
+        seq_dbutils.Database.commit_changes(mock_instance, True)
         mock_info.assert_called_with('Changes committed')
 
     @patch('logging.info')
@@ -32,7 +32,7 @@ class ConnectionTestClass(TestCase):
         pwd = 'password'
         host = 'myhost'
         db = 'mydb'
-        connection = seq_dbutils.Connection(user, pwd, host, db)
+        connection = seq_dbutils.Database(user, pwd, host, db)
         connection.create_sql_engine()
         mock_info.assert_called_with(f'Connecting to {db} on host {host}')
         mock_create.assert_called_once()
@@ -47,6 +47,6 @@ class ConnectionTestClass(TestCase):
         host = 'myhost'
         db = 'mydb'
         mock_create.side_effect = Mock(side_effect=Exception('Test exception'))
-        connection = seq_dbutils.Connection(user, pwd, host, db)
+        connection = seq_dbutils.Database(user, pwd, host, db)
         connection.create_sql_engine()
         mock_error.assert_called_with('Test exception')
